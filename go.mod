0bf1d7390561cef124f1f5f2cbff05af6b7df7dc
wipdev-tech <wipdev.tech@gmail.com>
2024-01-03 15:15:09 +0200

    All assignments for chapter 2: Routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wipdev-tech/chirpy
 
 go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.11 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,44 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+var cfg apiConfig
 
 func main() {
+
 	fs := http.FileServer(http.Dir("."))
-	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", fs))
-	mux.HandleFunc("/healthz", handleHealth)
 
-	corsMux := middlewareCors(mux)
+	// mux := http.NewServeMux()
+	r := chi.NewRouter()
+	r.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app/", fs)))
+	r.Handle("/app", cfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
+	r.Get("/healthz", handleHealth)
+	r.Get("/metrics", handleMetrics)
+	r.HandleFunc("/reset", handleReset)
 
-	s := &http.Server{
+	corsMux := middlewareCors(r)
+
+	s := http.Server{
 		Addr:    ":8080",
 		Handler: corsMux,
 	}
@@ -31,6 +59,19 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func handleHealth(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+}
+
+func handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
