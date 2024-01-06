8ec792a32033f054171a02dc62ca8ee87c959d92
wipdev-tech <wipdev.tech@gmail.com>
2024-01-07 00:57:54 +0200

    All assignments for chapter 4: JSON

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -31,6 +33,7 @@ func main() {
 	apiRouter := chi.NewRouter()
 	apiRouter.Get("/healthz", handleHealth)
 	apiRouter.HandleFunc("/reset", handleReset)
+	apiRouter.Post("/validate_chirp", handleValidate)
 
 	adminRouter := chi.NewRouter()
 	adminRouter.Get("/metrics", handleMetrics)
@@ -84,4 +87,43 @@ func handleReset(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func handleValidate(w http.ResponseWriter, r *http.Request) {
+	type msg struct {
+		Body string
+	}
+
+	type response struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	inMsg := msg{}
+	json.NewDecoder(r.Body).Decode(&inMsg)
+
+	if len(inMsg.Body) <= 140 {
+		inFields := strings.Fields(inMsg.Body)
+		for i, f := range inFields {
+			lower := strings.ToLower(f)
+			if lower == "kerfuffle" || lower == "sharbert" || lower == "fornax" {
+				inFields[i] = "****"
+			}
+		}
+		cleaned := strings.Join(inFields, " ")
+
+		resp := response{
+			CleanedBody: cleaned,
+		}
+
+		respBody, err := json.Marshal(resp)
+		if err != nil {
+			panic(err)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(respBody)
+		return
+	}
+
+	w.WriteHeader(http.StatusBadRequest)
 }
